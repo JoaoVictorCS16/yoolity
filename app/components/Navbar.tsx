239c82a48c65d7e8f007625377241a20710529ae
JoaoVictorCS16 <joaovictor.dev16@gmail.com>
2024-08-28 23:56:37 -0300

    Initial Commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { ThemeToggle } from "./Themetoggle";
+import { Button } from "@/components/ui/button";
+
+export function Navbar() {
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-3xl">Yoolity - CRM</h1>
+        </Link>
+
+        <div className="flex items-center gap-x-5">
+          <ThemeToggle />
+          <div className="flex items-center gap-x-5">
+            <Button>Entrar</Button>
+            <Button>Cadastrar</Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+//  Inicio do projeto: 16:57 (minutos)
